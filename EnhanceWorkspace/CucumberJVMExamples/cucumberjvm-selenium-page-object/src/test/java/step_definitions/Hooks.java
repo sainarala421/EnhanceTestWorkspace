19d5949961c8133dcdf5f6068114f92589b46bf0
Francis John Agunday <jarvis2@Franciss-MacBook-Pro.local>
2018-06-08 21:40:09 +1200

    Updated repo :).

--- EnhanceWorkspace/CucumberJVMExamples/cucumberjvm-selenium-page-object/src/test/java/step_definitions/Hooks.java
+++ EnhanceWorkspace/CucumberJVMExamples/cucumberjvm-selenium-page-object/src/test/java/step_definitions/Hooks.java
@@ -49,5 +49,12 @@ public class Hooks{
         driver.quit();
         
     }
+    public void analyzeLog() {
+        LogEntries logEntries = driver.manage().logs().get(LogType.BROWSER);
+        for (LogEntry entry : logEntries) {
+            System.out.println(new Date(entry.getTimestamp()) + " " + entry.getLevel() + " " + entry.getMessage());
+            //do something useful with the data
+        }
+    }
     
 }
